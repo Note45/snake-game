bf3a589693117fa9daa43c1d6ab0dc192976c4a4
Note45 <antonion.2048@gmail.com>
2021-11-20 10:31:03 -0300

    Fix some bugs

--- main.cpp
+++ main.cpp
@@ -34,16 +34,19 @@ float gridX, gridY;
 float posX[30] = {0,0,0,0,0}, posY[30] = {0,-1,-2,-3,-4};
 
 
-void initGL() {
+void initGlut() {
    glClearColor(0.4f, 0.4f, 0.4f, 0.4f); 		       // Seta a cor de fundo
    glClearDepth(1.0f);                                 // Seta o tamanho do background
    glEnable(GL_DEPTH_TEST);  			               // Enable depth testing for z-culling
    glDepthFunc(GL_LEQUAL);    						   // Set the type of depth-test
-   glShadeModel(GL_SMOOTH);   						   // Enable smooth shading
+   glShadeModel(GL_SMOOTH);   						   // Enable smooth shading					
    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
+   // Adiciona cores e sombras
+   glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);     
+   glEnable(GL_LIGHTING);     
+   glEnable(GL_LIGHT0);	
+   glEnable(GL_COLOR_MATERIAL);
 }
- 
-   
 
 void grid(float gridX,float gridY) {
 	gridX = -29;
@@ -55,17 +58,14 @@ void grid(float gridX,float gridY) {
 	//Chão
 	for(int i = -28;i < 33;i++){
 		for(int j = 29; j > -30;j--){
-				
 			glLoadIdentity();                 
-			glTranslatef(i, j, -91.0f); 			   
-								             
+			glTranslatef(i, j, -91.0f); 			   				             
 			glColor3f(0.0, 0.0, 0.0);
 			glRotatef (0.0, 50.0, 50.0, 1.0);
 			glPushMatrix();
 			glScalef (1.0, 1.0, 1.0);
 			glutSolidCube (1.0);
 			glPopMatrix();
-				
 		}
 	}
 	
@@ -86,8 +86,7 @@ void grid(float gridX,float gridY) {
 		 
 	}
 	
-	for(int i = 0;i < 61;i++){
-		
+	for(int i = 0;i < 61;i++) {
 		glLoadIdentity();                 
 		glTranslatef(gridX, gridY, -90.0f); 
 		
@@ -99,11 +98,9 @@ void grid(float gridX,float gridY) {
 		glPopMatrix();
 	
 		gridX = gridX + 1.0;
-		 
 	}
 	
 	for(int i = 0;i < 60;i++){
-		
 		glLoadIdentity();                 
 		glTranslatef(gridX, gridY, -90.0f); 
 			    
@@ -113,13 +110,11 @@ void grid(float gridX,float gridY) {
 		glScalef (1.0, 1.0, 1.0);
 		glutSolidCube(1.0);
 		glPopMatrix();
-		
+
 		gridY = gridY + 1.0;
-		 
 	}
 	
 	for(int i = 0;i < 62;i++){
-		
 		glLoadIdentity();                 
 		glTranslatef(gridX, gridY, -90.0f); 
 					   
@@ -131,10 +126,9 @@ void grid(float gridX,float gridY) {
 		glPopMatrix();
 			
 		gridX = gridX - 1.0;
-		 
 	}
 	
-	//Casa
+	//Casa da cobra
 	glLoadIdentity();                 
 	glTranslatef(1, -3, -89.0f); 
 						   
@@ -145,8 +139,8 @@ void grid(float gridX,float gridY) {
 	glutSolidCube (1.0);
 	glPopMatrix();
 	
-	//Telhado
-	glLoadIdentity();
+	//Telhado da casa
+ 	glLoadIdentity();
 	glTranslatef(1, -3, -88.0f); 
 						   
 	glColor3f(0.7, 0.7, 0.0);
@@ -156,105 +150,81 @@ void grid(float gridX,float gridY) {
 	glutSolidCone(1.0,1.0,20,1);
 	glPopMatrix();
 	
-//Árvores-------------------------
+//Pilastras-------------------------
 	glLoadIdentity();                 
 	glTranslatef(20, -20, -88.0f); 
-						   
+ 						  
 	glColor3f(0.6, 0.6, 0.0);
 	glRotatef (0.0, 50.0, 50.0, 1.0);
 	glPushMatrix();
 	glScalef (2.0, 3.0, 1.0);
 	glutSolidCube (1.0);
-	glPopMatrix();
-	
-	glLoadIdentity();     
-	glColor3f(0.0,0.8,0.0);
-	glPushMatrix();
-	glTranslated(20, -18, -88.0f); 
-	glRotated(0,0,0,1);
-	glScalef (1.0, 1.0, 1.0);
-	glutSolidSphere(2,10,10);
 	glPopMatrix();
 //---------------------------------
 	glLoadIdentity();                 
 	glTranslatef(19, 6, -88.0f); 
 						   
-	glColor3f(0.6, 0.6, 0.0);
+	glColor3f(0.8, 1.6, 0.5);
 	glRotatef (0.0, 50.0, 50.0, 1.0);
 	glPushMatrix();
 	glScalef (2.0, 3.0, 1.0);
 	glutSolidCube (1.0);
-	glPopMatrix();
-	
-	glLoadIdentity();     
-	glColor3f(0.0,0.8,0.0);
-	glPushMatrix();
-	glTranslated(19, 8, -88.0f); 
-	glRotated(0,0,0,1);
-	glScalef (1.0, 1.0, 1.0);
-	glutSolidSphere(2,10,10);
 	glPopMatrix();
 //---------------------------------
 	glLoadIdentity();                 
 	glTranslatef(-12, 8, -88.0f); 
 						   
-	glColor3f(0.6, 0.6, 0.0);
+	glColor3f(1, 0.6, 1);
 	glRotatef (0.0, 50.0, 50.0, 1.0);
 	glPushMatrix();
 	glScalef (2.0, 3.0, 1.0);
 	glutSolidCube (1.0);
-	glPopMatrix();
-	
-	glLoadIdentity();     
-	glColor3f(0.0,0.8,0.0);
-	glPushMatrix();
-	glTranslated(-12, 10, -88.0f); 
-	glRotated(0,0,0,1);
-	glScalef (1.0, 1.0, 1.0);
-	glutSolidSphere(2,10,10);
 	glPopMatrix();
 //---------------------------------
 	glLoadIdentity();                 
 	glTranslatef(-23, -14, -88.0f); 
 						   
-	glColor3f(0.6, 0.6, 0.0);
+	glColor3f(0.6, 1, 1);
 	glRotatef (0.0, 50.0, 50.0, 1.0);
 	glPushMatrix();
 	glScalef (2.0, 3.0, 1.0);
 	glutSolidCube (1.0);
-	glPopMatrix();
-	
-	glLoadIdentity();     
-	glColor3f(0.0,0.8,0.0);
-	glPushMatrix();
-	glTranslated(-23, -12, -88.0f); 
-	glRotated(0,0,0,1);
-	glScalef (1.0, 1.0, 1.0);
-	glutSolidSphere(2,10,10);
 	glPopMatrix();
 //---------------------------------
 	glLoadIdentity();                 
 	glTranslatef(5, 26, -88.0f); 
 						   
-	glColor3f(0.6, 0.6, 0.0);
+	glColor3f(0.0, 1.5, 1.0);
 	glRotatef (0.0, 50.0, 50.0, 1.0);
 	glPushMatrix();
 	glScalef (2.0, 3.0, 1.0);
-	glutSolidCube (1.0);
+	glutSolidCube(1.0);
 	glPopMatrix();
-	
-	glLoadIdentity();     
-	glColor3f(0.0,0.8,0.0);
+
+//---------------------------------
+	glLoadIdentity();                 
+	glTranslatef(10, 5, -88.0f); 
+						   
+	glColor3f(1.0, 1.0, 1.0);
+	glRotatef (0.0, 50.0, 50.0, 1.0);
 	glPushMatrix();
-	glTranslated(5, 28, -88.0f); 
-	glRotated(0,0,0,1);
-	glScalef (1.0, 1.0, 1.0);
-	glutSolidSphere(2,10,10);
+	glScalef (2.0, 3.0, 1.0);
+	glutSolidCube(1.0);
+	glPopMatrix();
+
+//---------------------------------
+	glLoadIdentity();                 
+	glTranslatef(5, 15, -88.0f); 
+						   
+	glColor3f(1.5, 1.0, 0.5);
+	glRotatef (0.0, 50.0, 50.0, 1.0);
+	glPushMatrix();
+	glScalef (2.0, 3.0, 1.0);
+	glutSolidCube(1.0);
 	glPopMatrix();
 
 	gridX = 0;
 	gridY = 0;
-	
 }
 
 void random(int &x,int &y){
@@ -264,7 +234,6 @@ void random(int &x,int &y){
 	srand(time(NULL));
 	x = _min + rand() % (_maxX - _min);
 	y = _min + rand() % (_maxY - _min);
-	
 }
 
 void drawFood() {
@@ -282,11 +251,10 @@ void drawFood() {
 	glPushMatrix();
 	glTranslated(foodX + 1, foodY - 2 , -90.0f); 
 	glRotated(a,0,0,1);
-	glScalef (1.1, 1.1, 1.0);
+	glScalef (1.1, 1.2, 1.0);
 	glutSolidSphere(1,100,7);
 	glPopMatrix();
 	glRectf(foodX,foodY,foodX+1,foodY+1);
-	
 }
 
 void snake() {
@@ -307,25 +275,20 @@ void snake() {
 	
 	for(int i = 0;i < snake_lenght;i++){
 		if(i == 0){
-			
 			//Cabeça
 			glLoadIdentity();                 
 			glTranslatef(gridX, gridY, -90.0f); 
 						             
-			glColor3f(0.0,1.0,1.0);
+			glColor3f(1.0,1.0, 0.0);
 			glTranslatef (1.0, -3.0, 0.0);
 			glPushMatrix();
 			glutSolidCube (1.0);
 			glPopMatrix();
-
-		}
-		else
-		{
+		} else {
 			//Corpo
 			glLoadIdentity();           
             glTranslatef(gridX, gridY, -90.0f);  
-			 
-            glColor3f(0.0f, 0.5f, 0.5f);
+            glColor3f(1.0f, 1.5f, 0.5f);
 			glTranslatef (1.0, -3.0, 0.0);
 			glPushMatrix();
 			glScalef (1.0, 1.0, 1.0);
@@ -334,7 +297,6 @@ void snake() {
 		}
 		
 		glRectd(posX[i],posY[i],posX[i]+1,posY[i]+1);
-		
 	}
 	
 	if(posX[0] == 30 || posY[0] == 32||posX[0] == -30||posY[0] == -27||(posX[0] == 20 && posY[0] == -20)||(posX[0] == 19 && posY[0] == 7)||(posX[0] == -13 && posY[0] == 9)||(posX[0] == -24 && posY[0] == -13)||(posX[0] == 4 && posY[0] == 28)){
@@ -355,7 +317,6 @@ void snake() {
     	if(posX[j] == posX[0] && posY[j] == posY[0])
 			gameOver=true;
 	}
-	
 }
 
  
@@ -382,15 +343,15 @@ void reshape(GLsizei width, GLsizei height) {
    glViewport(0, 0, width, height);
    glMatrixMode(GL_PROJECTION);  
    glLoadIdentity();             						//Reseta
-   gluPerspective(55.0f, 1, 0.5f, 100.0f);
-   gluLookAt(0,0,2,
-   	   	   	 0.0,0.3,0,    	  
+   gluPerspective(45.0f, aspect, 1.5f, 100.0f);
+   gluLookAt(0,0,1,
+   	   	   	 0,0.05,0,    	  
 			 0,1,0);
 }
 
-void timer_callback(int){
+void timerCallback(int){
 	glutPostRedisplay();
-	glutTimerFunc(1000/FPS,timer_callback,0);
+	glutTimerFunc(1000/FPS,timerCallback,0);
 }
 
 void keyboard_callback(int key,int,int){
@@ -412,21 +373,20 @@ void keyboard_callback(int key,int,int){
 				sDirection = LEFT;
 			break;
 	}
-	
 }
 
 
 int main(int argc, char** argv) {
    glutInit(&argc, argv);            			// Initialize GLUT
    glutInitDisplayMode(GLUT_DOUBLE); 			// Enable double buffered mode
-   glutInitWindowSize(700,700);   				// Set the window's initial width & height
+   glutInitWindowSize(780,780);   				// Set the window's initial width & height
    glutInitWindowPosition(100,100); 			// Position the window's initial top-left corner
    glutCreateWindow("Snake Game");  			// Create window with the given title
    glutDisplayFunc(display);       				// Register callback handler for window re-paint event
    glutReshapeFunc(reshape);  
-   glutTimerFunc(0,timer_callback,0);
+   glutTimerFunc(0, timerCallback, 0);
    glutSpecialFunc(keyboard_callback);     		// Register callback handler for window re-size event
-   initGL();                       				// Our own OpenGL initialization
+   initGlut();                       			// Our own OpenGL initialization
    PlaySound("base-music.wav", NULL, SND_ASYNC | SND_FILENAME | SND_LOOP);
    glutMainLoop();                 				// Enter the infinite event-processing loop
    return 0;
